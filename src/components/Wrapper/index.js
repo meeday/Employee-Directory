aa83fb788efa796a5a3ba1c4f39ec3d8eed58106
meeday <59993824+meeday@users.noreply.github.com>
2020-08-09 16:51:43 +0100

    wrapper comments added

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -1,5 +1,7 @@
+// react imported
 import React from "react";
-
+// wrapper function returns a div wrapper that holds props.children
 const wrapper = (props) => <div className="wrapper">{props.children}</div>;
 
 export default wrapper;
+// wrapper exported
