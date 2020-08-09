5e335878ad01303e26daf3425a31c559891201b4
meeday <59993824+meeday@users.noreply.github.com>
2020-08-09 16:44:44 +0100

    nav commentary added

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,7 +1,8 @@
+// React search component and styling imported
 import React from "react";
 import Search from "../search/index";
 import "./style.css";
-
+// nav function passes through the destructured handleinputChange to set the handleInputChange prop. returns a nav bar with the search bar
 const Nav = ({ handleInputChange }) => {
   return (
     <nav className="navbar navbar-expand navbar-dark bg-dark">
