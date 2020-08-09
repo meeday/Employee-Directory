d3ab4e0ff6490b3df61c463c394d349c1ddab403
meeday <59993824+meeday@users.noreply.github.com>
2020-08-09 16:53:25 +0100

    app comments added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+// import react and the wrapper, header, and employeeData components
 import React from "react";
 import Wrapper from "./components/Wrapper/index";
 import Header from "./components/header/index";
 import EmployeeData from "./components/EmployeeData/index";
-
+// app returns the layout of the page using the components 
 const App = () => {
   return (
     <Wrapper>
@@ -13,3 +14,4 @@ const App = () => {
 };
 
 export default App;
+// app exported
